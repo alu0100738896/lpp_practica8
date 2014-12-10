1deb1c4625c2acead23dd3d08839c76a13d53664
alu01007688932 <josedietadelcucurucho@hotmail.com>
2014-12-10 17:54:44 +0000

    Creada la espectativa que requiere a Quiz

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -1,7 +1,21 @@
+# encoding: utf-8
+
 require 'indice'
 require 'spec_helper'
 
 describe Quiz do
+    before :each do
+       quiz = Quiz.new("Cuestionario LPP") do
+            question '¿Cuantos argumentos de tipo bloque se le pueden pasar a un metodo?',
+            wrong: '0',
+            right: '1',
+            wrong: '2'
+        
+            question '¿Ruby es un lenguaje interpretado?',
+            wrong: 'Si',
+            right: 'No'
+        
+    end
     
     
 end
