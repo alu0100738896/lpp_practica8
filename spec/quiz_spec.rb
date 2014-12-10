fbdb18f1d2d74f5c5d2172565b5d5a49eeb8ef64
alu01007688932 <josedietadelcucurucho@hotmail.com>
2014-12-10 18:07:08 +0000

    añadida espectativa que muestra el test

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 
 describe Quiz do
     before :each do
-       quiz = Quiz.new("Cuestionario LPP") do
+       @quiz = Quiz.new("Cuestionario LPP") do
             question '¿Cuantos argumentos de tipo bloque se le pueden pasar a un metodo?',
             wrong: '4',
             right: '1',
@@ -14,7 +14,15 @@ describe Quiz do
             question '¿Ruby es un lenguaje interpretado?',
             wrong: 'Si',
             right: 'No'
+        end
+    end
+    
+    describe "probando la clase quiz" do
+        it "Comprobando que se imprima el test" do
         
+            expect(@quiz.to_s).to eq("Cuestionario LPP\n================\n\n1) ¿Cuantos argumentos de tipo bloque se le pueden pasar a un metodo?\n\t1) 1\n\t2) 2\n2) ¿Ruby es un lenguaje interpretado?\n\t1) No\n\t2) Si")    
+    
+        end
     end
     
     
