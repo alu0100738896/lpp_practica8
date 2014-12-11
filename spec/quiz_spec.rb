f4f948af804884749a2c8dd2ed6abae07c35e4ad
Miguel Ángel Delgado Hernández <alu0100738896@ull.edu.es>
2014-12-11 16:30:25 +0000

    editando bloque que se pasa

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -20,11 +20,11 @@ describe Quiz do
     describe "probando la clase quiz" do
         it "Comprobando que se imprima el test" do
         
-            expect(@quiz.to_s).to eq("Cuestionario LPP\n================\n\n1) ¿Cuantos argumentos de tipo bloque se le pueden pasar a un metodo?\n\t1) 1\n\t2) 2\n2) ¿Ruby es un lenguaje interpretado?\n\t1) No\n\t2) Si\n")    
+            expect(@quiz.to_s).to eq("Cuestionario LPP\n================\n\n1) ¿Cuantos argumentos de tipo bloque se le pueden pasar a un metodo?\n\t1) 4\n\t2) 1\n\t3) 2\n2) ¿Ruby es un lenguaje interpretado?\n\t1) No\n\t2) Si\n")    
     
         end
         it "corregir un test" do
-            expect(@quiz.run(["1","Si"])).to eq("Cuestionario LPP\n================\n\n1) ¿Cuantos argumentos de tipo bloque se le pueden pasar a un metodo?\n\t1) 1\n\t2) 2\nRespuesta: 1\n2) ¿Ruby es un lenguaje interpretado?\n\t1) No\n\t2) Si\nRespuesta: Si\nPregunta 1 correcta.\nPregunta 2 incorrecta.\nNota: Aprobado.\n")
+            expect(@quiz.run(["1","Si"])).to eq("Cuestionario LPP\n================\n\n1) ¿Cuantos argumentos de tipo bloque se le pueden pasar a un metodo?\n\t1) 4\n\t2) 1\n\t3) 2\nRespuesta: 1\n2) ¿Ruby es un lenguaje interpretado?\n\t1) No\n\t2) Si\nRespuesta: Si\nPregunta 1 correcta.\nPregunta 2 incorrecta.\nNota: Aprobado.\n")
         end
     end
     
