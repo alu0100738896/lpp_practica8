7039edae0b6b803552a6b7af43adbbe90826ab3e
Miguel Ángel Delgado Hernández <alu0100738896@ull.edu.es>
2014-12-17 17:29:19 +0000

    Definido el initialize

--- lib/naranjero.rb
+++ lib/naranjero.rb
@@ -1,5 +1,15 @@
 class Naranjero
     
-    
+	attr_reader :altura, :edad, :vivo
+	attr_accessor :naranjas
+
+	def initialize altura,edad
+		@altura, @edad = altura, edad
+		@naranjas = 0
+		@max_edad = 20
+		@adulto = 4
+		@produccion = []
+		@mutex= Mutex.new
+	end
     
 end
