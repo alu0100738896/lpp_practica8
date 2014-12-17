54affab84b4cc929c412c0694404ac386e41236d
Miguel Ángel Delgado Hernández <alu0100738896@ull.edu.es>
2014-12-17 17:35:38 +0000

    Definido metodos

--- lib/naranjero.rb
+++ lib/naranjero.rb
@@ -11,5 +11,37 @@ class Naranjero
 		@produccion = []
 		@mutex= Mutex.new
 	end
+	
+	def uno_mas	
+		if @edad <= @max_edad
+			@edad += 1
+			@altura += 0.5
+			if @edad.between?(4,10)
+				@mutex.synchronize { @naranjas += 5	}
+				@produccion << 5
+			elsif @edad.between?(11,20)
+				@mutex.synchronize { @naranjas += 8 }
+				@produccion << 8
+			end
+		else
+			@mutex.synchronize { @naranjas = 0 }
+			@altura = 0
+		end
+	end
+
+	def recolectar_una
+		if @naranjas > 0
+			@mutex.synchronize { @naranjas -= 1 }
+		end
+		if @edad >= @max_edad
+			@mutex.synchronize { @naranjas = 0 }
+			puts "El naranjo ha muerto."
+		elsif @naranjas == 0
+			puts "No hay naranjas."
+		else
+			puts "Estaba muy rica."
+		end
+	end
+end	
     
 end
